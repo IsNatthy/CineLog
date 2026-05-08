1a87c102d834fee1a12fa05e317ef00f7e9c774a
FlinkLZx <juanfeartunduaga914@gmail.com>
2026-05-08 18:54:54 -0500

    feature: Added values in Users.java

--- backend/src/main/java/com/example/backend/entities/Users.java
+++ backend/src/main/java/com/example/backend/entities/Users.java
@@ -1,4 +1,36 @@
-package main.java.com.example.backend.entities;
+package com.example.backend.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "surname",  nullable = false)
+    private String surname;
+    @Column(name = "email",  nullable = false)
+    private String email;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "birthday",  nullable = false)
+    private LocalDate birthday;
+
+    @Column(name = "createdAt", nullable = false)
+    private LocalDate createdAt;
+    @Column(name = "modifiedAt")
+    private LocalDate modifiedAt;
 }
