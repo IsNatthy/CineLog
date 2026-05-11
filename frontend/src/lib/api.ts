92be84bb3938f230f5da155ef12ea91e6a82cd6d
Nathalia Avila <nathyavila05@gmail.com>
2026-05-11 17:43:37 -0500

    Add auth helpers and axios API client

---
+++ frontend/src/lib/api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import { getToken, removeToken } from './auth'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080/api',
+})
+
+api.interceptors.request.use((config) => {
+  const token = getToken() 
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      removeToken()
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
