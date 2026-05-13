9cf5ea9f407dcc5aab1049f541b0181f076f66be
FlinkLZx <juanfeartunduaga914@gmail.com>
2026-05-12 23:13:37 -0500

    feature: minor fixes in users & diary repositories

--- backend/src/main/java/com/example/backend/repositories/DiaryEntryRepository.java
+++ backend/src/main/java/com/example/backend/repositories/DiaryEntryRepository.java
@@ -1,13 +1,7 @@
 package com.example.backend.repositories;
 
 import com.example.backend.entities.DiaryEntry;
-import com.example.backend.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface DiaryEntryRepository extends JpaRepository<DiaryEntry,Long> {
-
-    Optional<DiaryEntry> findByUserWatchList(Users users);
-
 }
--- backend/src/main/java/com/example/backend/repositories/UsersRepository.java
+++ backend/src/main/java/com/example/backend/repositories/UsersRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Users, Long> {
-
-    Optional<Users> findByUser(Long id);
+public interface UsersRepository extends JpaRepository<Users, Long> {
 
 }
